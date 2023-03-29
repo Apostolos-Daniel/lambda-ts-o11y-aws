86ee358c8b0fbc364a33f7ceea92774713f92afb
Toli <dev@toli.io>
2023-03-29 14:50:31 +0100

    Initial commit

---
+++ test/lambda-ts-o11y-aws.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LambdaTsO11YAws from '../lib/lambda-ts-o11y-aws-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lambda-ts-o11y-aws-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LambdaTsO11YAws.LambdaTsO11YAwsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
